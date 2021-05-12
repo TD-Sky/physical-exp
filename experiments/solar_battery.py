1ba0fe530a4c028f6042fe25e2bdf699442ad85d
randal-inspiron <1752963246@qq.com>
2021-05-12 21:47:54 +0800

    加入画图功能，为什么画出来一条指数函数曲线?

--- experiments/solar_battery.py
+++ experiments/solar_battery.py
@@ -1,4 +1,7 @@
-import pprint
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+import numpy as np
+from sklearn.linear_model import LinearRegression
 from .experiment import Experiment
 
 
@@ -11,6 +14,8 @@ class Solar_battery(Experiment):
         self.output = "太阳能电池基本特性的测量.txt"
         self.data = {}
         self.result = {}
+
+        plt.style.use('classic')
     
 
     def collect_data(self):
@@ -23,8 +28,8 @@ class Solar_battery(Experiment):
         lines = [ [float(s) for s in line.split()] for line in raw_data.splitlines() if line != '' ]
         cols = list(zip(*lines))
         self.data['R'] = tuple([ int(x) for x in cols[0] ])
-        self.data['I/mA'] = cols[1]
-        self.data['U/V'] = cols[2]
+        self.data['I/mA'] = np.array(cols[1])
+        self.data['U/V'] = np.array(cols[2])
 
 
     def process(self):
@@ -38,18 +43,20 @@ class Solar_battery(Experiment):
         # 获取 最大输出功率及相应电阻值
         # 与 绘制电阻-功率表格 二合一
         R_P = 'R/Ω\t\tP/mW\n'
-        Pmax = corespond_R = 0
+        Pmax = cores_R = 0
         for R, P in zip(self.data['R'], self.result['P/mW']):
             R_P += f'{R}\t\t{self.round_dec(P, 3)}\n'
             if Pmax < P:
                 Pmax = P
-            corespond_R = R
+                cores_R = R
         else:
             R_P += '\n'
 
         self.Ostream('output', self.output,
                 R_P + 
                 f"最大输出功率: {self.round_dec(Pmax, 3)}\n" + 
-                f"相应电阻值: {corespond_R}\n\n")
+                f"相应电阻值: {cores_R}\n\n")
 
+        plt.scatter(self.data['U/V'], self.data['I/mA'])
+        plt.show()
 
