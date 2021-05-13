761c5f000813efa5882dfb9bb01fa6437870c34e
randal-inspiron <1752963246@qq.com>
2021-05-13 10:51:29 +0800

    改进代码

--- experiments/solar_battery.py
+++ experiments/solar_battery.py
@@ -9,32 +9,39 @@ class Solar_battery(Experiment):
 
     def __init__(self):
         self.template = "solar_battery.txt"
-        self.input = "太阳能电池基本特性的测量.txt"
-        self.output = "太阳能电池基本特性的测量.txt"
+        self.io = "太阳能电池基本特性的测量.txt"
         self.data = {}
         self.result = {}
 
         plt.style.use('classic')
+        self.fig = plt.figure()
     
 
     def collect_data(self):
         """ """
+        code = 0
         try:
-            raw_data = self.Istream('input', self.input)
+            raw_data = self.Istream('input', self.io)
         except IOError:
-            return -1
-
-        lines = [ [float(s) for s in line.split()] for line in raw_data.splitlines() if line != '' ]
-        cols = list(zip(*lines))
-        self.data['R'] = tuple([ int(x) for x in cols[0] ])
-        self.data['I/mA'] = np.array(cols[1])
-        self.data['U/V'] = np.array(cols[2])
+            code = -1
+        else:
+            lines = [ line.split() for line in raw_data.splitlines() if line != '' ]
+            cols = list(zip(*lines))
+            self.data['R'] = tuple([ int(s) for s in cols[0] ])
+            self.data['I/mA'] = np.array([ float(s) for s in cols[1] ])
+            self.data['U/V'] = np.array([ float(s) for s in cols[2] ])
 
 
     def process(self):
         """ """
         Ps = [ I*U for I, U in zip(self.data['I/mA'], self.data['U/V']) ]
         self.result['P/mW'] = [self.round_dec(P, 3) for P in Ps]
+
+        # 设置曲线图样式
+        plt.xticks(np.arange(0, 3, 0.1))
+        plt.yticks(np.arange(0, 3, 0.1))
+        plt.grid()
+        plt.scatter(self.data['U/V'], self.data['I/mA'])
 
 
     def write_result(self):
@@ -51,16 +58,11 @@ class Solar_battery(Experiment):
         else:
             R_P += '\n'
 
-        self.Ostream('output', self.output,
+        self.Ostream('output', self.io,
                 R_P + 
                 f"最大输出功率: {self.round_dec(Pmax, 3)}\n" + 
-                f"相应电阻值: {cores_R}\n\n")
+                f"相应电阻值: {cores_R}\n"
+                "手算填充因子，请：\n\n")
 
-        fig = plt.figure()
-        file = os.path.join(self.getPrefix(), 'output', '太阳能电池伏安特性曲线.png')
-        plt.xticks(np.arange(0, 3, 0.1))
-        plt.yticks(np.arange(0, 3, 0.1))
-        plt.grid()
-        plt.scatter(self.data['U/V'], self.data['I/mA'])
-        fig.savefig(file)
+        self.fig.savefig(os.path.join(self.getPrefix(), 'output', '太阳能电池伏安特性曲线.png'))
 
