611d88c4576d64da1c680df302890b47f61feb70
Randal <1752963246@qq.com>
2021-06-15 09:07:56 +0800

    写入功能

--- experiments/e_oscilloscope.py
+++ experiments/e_oscilloscope.py
@@ -22,6 +22,17 @@ class E_oscilloscope(Experiment):
         """ """
         self.result["period"] = np.divide(self.data["complete_waves"] * self.data["x_intervals"], self.data["frequency"])
         self.result["peak_peak"] = self.data["y_intervals"] * self.data["voltage_konb_gear"]
-        self.result["effect_vals"] = np.divide(self.result["peak_peak"], 8**0.5)
+
+        effect_vals = np.divide(self.result["peak_peak"], 8**0.5)
+        self.result["effect_vals"] = [str(self.round_dec(n, 2)) for n in effect_vals]
+
         self.result["correct_factors"] = np.absolute(self.data["cal_vals"] - self.data["indicate_vals"])
+
+
+    def write_result(self):
+        """ """
+        self.Ostream(f"周期: {self.result['period']}\n"
+                     f"峰-峰值: {self.result['peak_peak']}\n"
+                     f"有效值: {self.result['effect_vals']}\n"
+                     f"修正值: {self.result['correct_factors']}\n")
 
