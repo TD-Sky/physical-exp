2e24e231d841c68e12323cdba93ed2bee99df016
Randal <1752963246@qq.com>
2021-06-15 15:31:11 +0800

    非线性实验(废案)

--- experiments/__init__.py
+++ experiments/__init__.py
@@ -3,3 +3,4 @@ from .vernier_caliper import Vernier_caliper
 from .solar_battery import Solar_battery
 from .pn_junction import Pn_junction
 from .e_oscilloscope import E_oscilloscope
+from .non_linear import Non_linear
