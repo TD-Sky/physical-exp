60ce846bea460be188a7221b96a06db397308857
Randal <1752963246@qq.com>
2021-10-17 21:37:04 +0800

    命名数组改为类实现

--- experiments/pn_junction.py
+++ experiments/pn_junction.py
@@ -7,12 +7,19 @@ from .experiment import Experiment
 from .tools import getPrefix, round_dec
 
 
+class Matrl_const():
+
+    def __init__(self, model: LinearRegression):
+        self.a: float = float(round_dec(model.intercept_, 3))
+        self.k: str = f"{float(round_dec(-model.coef_[0], 5) * 1000)} × 10ˉ³"
+
+
 class Pn_junction(Experiment):
     """pn结温度-电压特性的测定"""
 
     def __init__(self):
         self.data: dict = {}
-        self.result
+        self.result: Matrl_const
 
         plt.style.use("classic")
         self.fig = plt.figure()
@@ -23,16 +30,12 @@ class Pn_junction(Experiment):
         self.data["U/V"] = np.array(raw_data["U_V"])
 
     def process(self):
-        Matrl_const = namedtuple("Matrl_const", ["a", "k"])
         # 线性回归
         model = LinearRegression(fit_intercept=True)
         reshape_t = self.data["t/C"][:, np.newaxis]
         model.fit(reshape_t, self.data["U/V"])
         self.Ufit = model.predict(reshape_t)
-        self.result = Matrl_const(
-            float(round_dec(model.intercept_, 3)),
-            f"{float(round_dec(-model.coef_[0], 5) * 1000)} × 10ˉ³",
-        )
+        self.result = Matrl_const(model)
 
         # 设置曲线图样式
         plt.xticks(np.arange(25, 80, 5))
@@ -55,4 +58,4 @@ class Pn_junction(Experiment):
         )
 
     def output(self) -> str:
-        return "a 与 k 是与pn结材料有关的常数\na = {}\nk = {}\n".format(self.result[0], self.result[1])
+        return "a 与 k 是与pn结材料有关的常数\na = {}\nk = {}\n".format(self.result.a, self.result.k)
